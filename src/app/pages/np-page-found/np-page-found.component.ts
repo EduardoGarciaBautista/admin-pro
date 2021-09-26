548c5c63326d89328504e1d783326de38225e5be
Eduardo Garcia Bautista <eduardo.garciab@hormail.com>
2021-09-25 20:33:31 -0500

    initial components

---
+++ src/app/pages/np-page-found/np-page-found.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-np-page-found',
+  templateUrl: './np-page-found.component.html',
+  styles: [
+  ]
+})
+export class NpPageFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
